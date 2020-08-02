f9dfd56b510a608c14521152d267203fd8fcdb8d
Sami Loone <sami@loone.fi>
2020-08-02 19:00:30 +0300

    repo: version bump to 0.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name="rsapi",
-    version="0.0.13",
+    version="0.0.14",
     author="ostracker.xyz",
     author_email="admin@ostracker.xyz",
     description="Python library for accessing Runescape APIs",
